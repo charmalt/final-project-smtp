7c54473d4b5cb559a569476393645e03e00f20d5
John Newman <newmanj903@gmail.com>
2018-06-26 13:41:50 +0100

    Adding jake tasks for creating databases

--- jake.js
+++ jake.js
@@ -1,10 +1,22 @@
-desc('Create main local Databse and table');
-task('mainDatabase', {async: true}, function () {
+//npm install -g jake
+
+//jake --jakefile jake.js makeDatabase
+
+desc('Create main local Databse');
+task('makeDatabase', {async: true}, function () {
   var cmds = [
-    'createdb mailbox',
-    ''
-
-
+    'createdb mailbox;'
+  ];
+  jake.exec(cmds, {printStdout: true}, function () {
+    console.log('All tests passed.');
+    complete();
+  });
+});
+// jake --jakefile jake.js testDatabase
+desc('Create main local test Databse');
+task('testDatabase', {async: true}, function () {
+  var cmds = [
+    'createdb testmailbox;'
   ];
   jake.exec(cmds, {printStdout: true}, function () {
     console.log('All tests passed.');
