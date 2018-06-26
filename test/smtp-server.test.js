da52eeec125ea7abfa386ee8f19b773d1685d449
benfurber <ben@furber-consultancy.com>
2018-06-26 15:25:14 +0100

    Server.start calls net.createServer

--- test/smtp-server.test.js
+++ test/smtp-server.test.js
@@ -1,18 +1,20 @@
 const Server = require('../lib/smtp-server')
+jest.mock('net')
 
 describe('Server', () => {
+  let net = require('net')
   let server
   let serverPort = 1337
   let serverAddress = '127.0.0.1'
 
   beforeEach(() => {
     server = new Server(serverPort, serverAddress)
+    net.createServer = () => { return 'Server'}
   })
 
   it('creates an TCP server', () => {
-    console.log = jest.fn()
     server.start()
-    expect(console.log.mock.calls[0][0]).toBe('Server started')
+    expect(server.server).toEqual('Server')
   })
 
   it('defines a port', () => {
