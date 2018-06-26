ec7f534f75cbf71e1f1f82fe5b38f8444611b14b
Charlene <charlenechetcuti@gmail.com>
2018-06-26 15:23:57 +0100

    Refactor smtp client handshake into a class

--- test/smtp_client_handshake_logic.test.js
+++ test/smtp_client_handshake_logic.test.js
@@ -1,22 +1,27 @@
-const smtpClientHandshake = require('../lib/smtp_client_handshake_logic')
+const SmtpClientHandshake = require('../lib/smtp_client_handshake_logic')
+
 
 
 describe('smtpClientHandshake module', function () {
 
-  it('should respond to \'EHLO\' with 250', function () {
-    expect(smtpClientHandshake('EHLO server')).toEqual(250)
+  beforeEach(function () {
+    smtpClientHandshake = new SmtpClientHandshake
   })
-  xit('should respond to \'HELO\' with 250', function () {
-    expect(smtpClientHandshake('HELO server')).toEqual(250)
+
+  it('should respond to \'EHLO\' with 250', function () {
+    expect(smtpClientHandshake.parseMessage('EHLO server')).toEqual(250)
+  })
+  it('should respond to \'HELO\' with 250', function () {
+    expect(smtpClientHandshake.parseMessage('HELO server')).toEqual(250)
   })
   xit('should respond to second \'HELO\' with ERROR', function () {
-    expect(smtpClientHandshake('HELO server')).toEqual(250)
-    expect(smtpClientHandshake('HELO server')).toEqual(503)
+    expect(smtpClientHandshake.parseMessage('HELO server')).toEqual(250)
+    expect(smtpClientHandshake.parseMessage('HELO server')).toEqual(503)
   })
   it('should respond to \'MAIL FROM: at@test.com\' with 250', function () {
-    expect(smtpClientHandshake('MAIL FROM: at@test.com')).toEqual(250)
+    expect(smtpClientHandshake.parseMessage('MAIL FROM: at@test.com')).toEqual(250)
   })
   it('should check if the message follows the correct order', function () {
-    
+
   })
 })
