9180f3677c52590e2120af51e3d5f88fd30e21e5
benfurber <ben@furber-consultancy.com>
2018-06-26 13:45:44 +0100

    Add address as constructor on Server

--- test/smtp-server.test.js
+++ test/smtp-server.test.js
@@ -3,9 +3,10 @@ const Server = require('../lib/smtp-server')
 describe('Server', () => {
   let server
   let serverPort = 1337
+  let serverAddress = '127.0.0.1'
 
   beforeEach(() => {
-    server = new Server(serverPort)
+    server = new Server(serverPort, serverAddress)
   })
 
   it('creates an TCP server', () => {
@@ -16,5 +17,9 @@ describe('Server', () => {
 
   it('defines a port', () => {
     expect(server.port).toBe(serverPort)
+  })
+
+  it('defines an address', () => {
+    expect(server.address).toBe(serverAddress)
   })
 })
