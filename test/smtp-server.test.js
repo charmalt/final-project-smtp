c026bd9eb0c1ce4c7a8ff7d1cb487177057d93b2
benfurber <ben@furber-consultancy.com>
2018-06-26 13:42:56 +0100

    Add port as constructor to Server

--- test/smtp-server.test.js
+++ test/smtp-server.test.js
@@ -2,14 +2,19 @@ const Server = require('../lib/smtp-server')
 
 describe('Server', () => {
   let server
+  let serverPort = 1337
 
   beforeEach(() => {
-    server = new Server()
+    server = new Server(serverPort)
   })
 
   it('creates an TCP server', () => {
     console.log = jest.fn()
     server.start()
     expect(console.log.mock.calls[0][0]).toBe('Server started')
+  })
+
+  it('defines a port', () => {
+    expect(server.port).toBe(serverPort)
   })
 })
