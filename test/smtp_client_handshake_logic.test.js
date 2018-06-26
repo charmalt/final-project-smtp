a730c591af1caeb13a986e721333cf1a23e81565
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-26 15:06:43 +0100

    Add tests for mailFrom input

--- test/smtp_client_handshake_logic.test.js
+++ test/smtp_client_handshake_logic.test.js
@@ -1,11 +1,22 @@
 const smtpClientHandshake = require('../lib/smtp_client_handshake_logic')
 
+
 describe('smtpClientHandshake module', function () {
+
   it('should respond to \'EHLO\' with 250', function () {
     expect(smtpClientHandshake('EHLO server')).toEqual(250)
   })
-  it('should respond to \'HELO\' with 250', function () {
+  xit('should respond to \'HELO\' with 250', function () {
     expect(smtpClientHandshake('HELO server')).toEqual(250)
-
+  })
+  xit('should respond to second \'HELO\' with ERROR', function () {
+    expect(smtpClientHandshake('HELO server')).toEqual(250)
+    expect(smtpClientHandshake('HELO server')).toEqual(503)
+  })
+  it('should respond to \'MAIL FROM: at@test.com\' with 250', function () {
+    expect(smtpClientHandshake('MAIL FROM: at@test.com')).toEqual(250)
+  })
+  it('should check if the message follows the correct order', function () {
+    
   })
 })
