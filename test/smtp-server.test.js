28ec50b2933cfe637e2bc3882ec78b4b91bf33f4
George Sykes <georgesykes86@gmail.com>
2018-06-27 09:10:26 +0100

    Add default TCPServer to SMTPServer

--- test/smtp-server.test.js
+++ test/smtp-server.test.js
@@ -1,19 +1,31 @@
 const SMTPServer = require('../lib/smtpServer')
+const TCPServer = require('../lib/tcpServer')
+jest.mock('../lib/tcpServer')
 
 describe('SMTPServer', () => {
   let server
+  let mockServer = {
+    start: jest.fn()
+  }
 
   beforeEach(() => {
     server = new SMTPServer()
+    TCPServer.mockImplementation((port, address) => {
+      return mockServer
+    })
   })
 
   describe('default behaviour', () => {
     it('has a port of 1337', () => {
-      expect(server.port).toEqual(1337)
+      expect(server.port).toBe(1337)
     })
 
     it('has a default address of 127.0.0.1', () => {
       expect(server.address).toEqual('127.0.0.1')
+    })
+
+    it('creates a TCPServer', () => {
+      expect(server.server).toBe(mockServer)
     })
   })
 })
