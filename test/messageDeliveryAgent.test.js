7ea0a75f8b2a445b6142d630aa605d82f28e63c0
George Sykes <georgesykes86@gmail.com>
2018-06-28 15:16:54 +0100

    Add error check for posting to the database

--- test/messageDeliveryAgent.test.js
+++ test/messageDeliveryAgent.test.js
@@ -3,28 +3,33 @@ const MessageDeliveryAgent = require('../lib/messageDeliveryAgent.js')
 describe('MessageDeliveryAgent', () => {
   let mda
   let databaseMock = {post: jest.fn()}
-  let databaseSpy
+  let databaseErrorMock = { post: jest.fn(() => { throw new Error('Error') }) }
   let queueMock
   let queueSpy
+  let emptyMock
+  let messageReplaceSpy
+  let databaseSpy
+
   beforeEach(() => {
-    queueMock = { takeFromQueue: jest.fn() }
+    queueMock = { takeFromQueue: jest.fn(), replaceInQueue: jest.fn() }
     queueSpy = jest.spyOn(queueMock, 'takeFromQueue')
       .mockReturnValue({
         mailFrom: 'john',
         rcptTo: 'george',
         messageBody: 'message body'})
+    messageReplaceSpy = jest.spyOn(queueMock, 'replaceInQueue')
     databaseSpy = jest.spyOn(databaseMock, 'post')
     mda = new MessageDeliveryAgent(databaseMock)
     databaseSpy.mockClear()
+    emptyMock = jest.fn()
+    emptyMock
+      .mockReturnValueOnce(false)
+      .mockReturnValueOnce(true)
+    queueMock.isEmpty = emptyMock
   })
 
   describe('queueNotEmpty', () => {
     it('on call takes message out of queue', () => {
-      let emptyMock = jest.fn()
-      emptyMock
-        .mockReturnValueOnce(false)
-        .mockReturnValueOnce(true)
-      queueMock.isEmpty = emptyMock
       mda.queueNotEmpty(queueMock)
       expect(queueSpy).toHaveBeenCalledTimes(1)
     })
@@ -41,13 +46,14 @@ describe('MessageDeliveryAgent', () => {
     })
 
     it('post arguments are coorect', () => {
-      let emptyMock = jest.fn()
-      emptyMock
-        .mockReturnValueOnce(false)
-        .mockReturnValueOnce(true)
-      queueMock.isEmpty = emptyMock
       mda.queueNotEmpty(queueMock)
       expect(databaseSpy).toHaveBeenCalledWith('john', 'george', 'message body')
+    })
+
+    it('returns message to the queue if unable to put in database', () => {
+      let newMDA = new MessageDeliveryAgent(databaseErrorMock)
+      newMDA.queueNotEmpty(queueMock)
+      expect(messageReplaceSpy).toHaveBeenCalledTimes(1)
     })
   })
 })
