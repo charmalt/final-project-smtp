b8384ceae5365bcb7d914b747367d81432720269
John Newman <newmanj903@gmail.com>
2018-06-29 16:13:28 +0100

    Add env variables to start server

--- startServer.js
+++ startServer.js
@@ -1,4 +1,6 @@
+// file requires .env.js to be configured, please see README for details.
+let ENV = require('./.env')
 let SMTPServer = require('./lib/SMTPServer')
-let server = new SMTPServer()
+let server = new SMTPServer(ENV['DEFAULT_PORT'], ENV['DEFAULT_ADDRESS'])
 
 server.start()
