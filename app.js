a405732a035133333193c0eff9f16f894334a29f
benfurber <ben@furber-consultancy.com>
2018-07-02 14:31:56 +0100

    Change the port for serverstart

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log(process.env)
 
 let SMTPServer = require('./lib/smtpServer')
-let serverName = new SMTPServer(process.env.SMTP_PORT, process.env.EC2_IP)
+let serverName = new SMTPServer(process.env.PORT, process.env.EC2_IP)
 
 serverName.start()
