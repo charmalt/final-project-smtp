0159e439ec3e6465af70c3981c1b9d3c9e79dc99
benfurber <ben@furber-consultancy.com>
2018-07-03 11:05:17 +0100

    Made table create a condition of env not being null

--- models/TableGenerator.js
+++ models/TableGenerator.js
@@ -5,9 +5,11 @@ const pg = require('pg')
 const dbConnections = [process.env.PGTEST, process.env.PGDEVELOPMENT]
 
 dbConnections.forEach((connection) => {
-  const client = new pg.Client(connection)
-  client.connect()
-  const query = client.query(
-    'CREATE TABLE mail(id SERIAL PRIMARY KEY, mailfrom VARCHAR(60), mailto VARCHAR(60), mailbody VARCHAR(200))')
-  query.on('end', () => { client.end() })
+  if (connection !== null) {
+    const client = new pg.Client(connection)
+    client.connect()
+    const query = client.query(
+      'CREATE TABLE mail(id SERIAL PRIMARY KEY, mailfrom VARCHAR(60), mailto VARCHAR(60), mailbody VARCHAR(200))')
+    query.on('end', () => { client.end() })
+  }
 })
