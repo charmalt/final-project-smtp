9946abe285516ac54005b1a5140933ec806c2a36
Charlene <charlenechetcuti@gmail.com>
2018-07-03 14:33:22 +0100

    Amend Jake for debugging

--- jake.js
+++ jake.js
@@ -1,9 +1,9 @@
 desc('Create main local Databse')
 task('makeLocalDatabases', { async: true }, function () {
   var cmds = [
-    'createdb mailbox;',
-    'createdb testmailbox;',
-    'node models/TableGenerator.js'
+     'createdb mailbox;'
+    // 'createdb testmailbox;',
+     // 'node models/TableGenerator.js'
   ]
   jake.exec(cmds, {printStdout: true}, function () {
     console.log('All tests passed.')
