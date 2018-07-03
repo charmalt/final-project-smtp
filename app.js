8e10134bf975740f5a768a63f0bd58b1e043985f
Igor Ryabchuk <riabtchuk@gmail.com>
2018-07-03 17:46:09 +0100

    Amend app.js starting script to include domain

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ let pg = require('pg')
 const env = require('./config')['production']
 
 console.log(env.dbConnectionString)
+let domain = 'test.com'
 let dbClient = new pg.Client(env.dbConnectionString)
 console.log(dbClient)
-let serverName = new SMTPServer(env.smtpPort, env.smtpHost, dbClient)
+let serverName = new SMTPServer(env.smtpPort, env.smtpHost, domain, dbClient)
 serverName.start()
