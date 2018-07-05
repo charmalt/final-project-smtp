b2b073ea1875ac72d8a53ac1a52790e00741c24c
George Sykes <georgesykes86@gmail.com>
2018-07-05 09:06:59 +0100

    Update travis.yml and app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let SMTPServer = require('./lib/smtpServer')
 let pg = require('pg')
 
-const env = require('./config')['production']
+const config = require('./config')['production']
 
 let domain = 'test.com'
-let dbClient = new pg.Client(env.dbConnectionString)
-let serverName = new SMTPServer(env.smtpPort, env.smtpHost, domain, dbClient)
+let dbClient = new pg.Client(config.dbConnectionString)
+let serverName = new SMTPServer(config.smtpPort, config.smtpHost, domain, dbClient)
 serverName.start()
--- config.js
+++ config.js
@@ -4,22 +4,16 @@ module.exports = {
   'development': {
     dbConnectionString: 'postgres://localhost:5432/mailbox',
     smtpPort: 1337,
-    smtpHost: '127.0.0.1',
-    popPort: 5001,
-    popHost: '127.0.0.1'
+    smtpHost: '127.0.0.1'
   },
   'test': {
     dbConnectionString: 'postgres://localhost:5432/testmailbox',
     smtpPort: 1337,
-    smtpHost: '127.0.0.1',
-    popPort: 5001,
-    popHost: '127.0.0.1'
+    smtpHost: '127.0.0.1'
   },
   'production': {
     dbConnectionString: process.env.PGPROD,
     smtpPort: 1337,
-    smtpHost: '127.0.0.1',
-    popPort: 5001,
-    popHost: '127.0.0.1'
+    smtpHost: '127.0.0.1'
   }
 }
